35232e3bc0b36a3652bfb60fceb818244aab5886
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-05-19 14:00:38 +0800

    feat: views setup

---
+++ app/Views/layouts/defaultDev.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $this->renderSection("title") ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+</head>
+
+<body>
+  <?= $this->renderSection("content") ?>
+</body>
+
+</html>
