e0e959abaea758ac6c9dda4979546d80a103bced
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-05-21 22:02:32 +0800

    feat: admin product menu initial ui

--- app/Views/pages/admin/products.php
+++ app/Views/pages/admin/products.php
@@ -6,20 +6,143 @@
 
 <?= $this->section("content") ?>
 
-<div class="row h-100 gy-3">
-  <div class="col-6 h-20">
-    <div class="h-100 w-100" style="background-color: gray;">
-
-    </div>
+<div class="row h-auto justify-content-between">
+  <div class="col-12">
+    <h1>Products</h1>
   </div>
-  <div class="col-6 h-20">
-    <div class="h-100 w-100" style="background-color: blue;">
-
-    </div>
+  <div class="col-2">
+    <button class="btn btn-primary align-self-center">Create New Product</button>
   </div>
-  <div class="col-12 h-80">
-    <div class="h-100 w-100" style="background-color: pink;">
+  <div class="col-2">
 
+    <label for="select-org" class="form-label">Filter by Organization</label>
+    <select id="select-org" class="form-select">
+      <option>None</option>
+      <option>One</option>
+      <option>Two</option>
+      <option>Three</option>
+    </select>
+  </div>
+
+</div>
+<div class="row h-100">
+
+  <div class="col-12">
+    <div class="table-responsive">
+      <table class="table table-dark table-hover table-bordered table-striped text-center">
+        <thead>
+          <tr>
+            <th scope="col">ID</th>
+            <th scope="col">Product Name</th>
+            <th scope="col">Price</th>
+            <th scope="col">Stock</th>
+            <th scope="col">Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+          <tr>
+            <td>#1</td>
+            <td>Phoenix Shirt</td>
+            <td>450</td>
+            <td>10</td>
+            <td class="text-right">
+              <button class="btn btn-primary badge-pill" style="width:80px;">EDIT</button>
+              <button class="btn btn-primary badge-pill" style="width:80px;">DELETE</button>
+            </td>
+          </tr>
+
+        </tbody>
+      </table>
     </div>
   </div>
 </div>
