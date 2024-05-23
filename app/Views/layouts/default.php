e713eefae4231d5c6aabe86844229dca1aac6ce6
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-05-23 19:17:46 +0800

    feat: user navbar initial ui

---
+++ app/Views/layouts/default.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title><?= $this->renderSection("title") ?></title>
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+  <link rel="stylesheet" href="<?= base_url() . 'global.css' ?>">
+</head>
+
+<body>
+  <div class="container">
+    <?= $this->include('partials/navbar.php'); ?>
+
+    <?= $this->renderSection("content") ?>
+  </div>
+
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+  <style>
+    body {
+      background-color: gray;
+    }
+  </style>
+</body>
+
+</html>
