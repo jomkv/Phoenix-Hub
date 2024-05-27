f7550ed8b36be1288894d91dfef51a15085cbff5
Rhondel Divinas Flores <rhondeldi@gmail.com>
2024-05-27 18:34:06 +0800

    product page item

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -41,7 +41,38 @@
     </div>
   </div>
 </div>
+<div class="container text-center p-5" style="margin-top: 100px; border-style:solid; background-color:violet;">
 
+  <div class="row align-items-center">
+    <div class="col" style="background-color:white; height: 300px;">
+      <div class="card" style="width: 18rem;">
+        <img src="<?= base_url() . 'White Tshirt.jpg'  ?>" class="img-thumbnail mx-auto d-block" alt="..." style="width:80%; ">
+        <div class="card-body">
+
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col" style="background-color:white; height: 300px;">
+      <div class="card" style="width: 18rem;">
+        <img src="<?= base_url() . 'White Tshirt.jpg' ?>" class="img-thumbnail mx-auto d-block" alt="..." style="width:80%; ">
+        <div class="card-body">
+
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+    <div class="col" style="background-color:white; height: 300px;">
+      <div class="card" style="width: 18rem;">
+        <img src="<?= base_url() . 'White Tshirt.jpg' ?>" class="img-thumbnail mx-auto d-block" alt="..." style="width:80%; ">
+        <div class="card-body">
+
+          <a href="#" class="btn btn-primary">Go somewhere</a>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <div class="d-grid gap-2 col-6 mx-auto" style="padding:50px;">
 
   <button class="btn btn-primary mx-auto d-block" type="button" style="width:30%;">Barter</button>
--- app/Views/pages/product/productPage.php
+++ app/Views/pages/product/productPage.php
@@ -3,5 +3,24 @@
 <?= $this->section("title") ?>Product Menu <?= $this->endSection() ?>
 
 <?= $this->section("content") ?>
-
+<div class="card mb-3" style="margin-top:150px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="<?= base_url() . 'White Tshirt.jpg' ?>" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">On God</h5>
+        <p class="card-text">This t-shirt is made of God.</p>
+        <div class="col-md-8 mx-auto d-block" style="margin-top:200px;">
+        <a class="btn btn-primary" href="#" role="button">Link</a>
+            <button class="btn btn-primary" type="submit">Button</button>
+            <input class="btn btn-primary" type="button" value="Input">
+            <input class="btn btn-primary" type="submit" value="Submit">
+            <input class="btn btn-primary" type="reset" value="Reset">
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 <?= $this->endSection() ?>
