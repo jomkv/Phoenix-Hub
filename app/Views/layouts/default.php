1315a7923e6d3cf0da8339d9a972592578243afc
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-06-01 16:38:22 +0800

    feat: default layout center content

--- app/Views/layouts/default.php
+++ app/Views/layouts/default.php
@@ -7,14 +7,15 @@
   <title><?= $this->renderSection("title") ?></title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-  <link rel="stylesheet" href="<?= base_url() . 'global.css' ?>">
 </head>
 
-<body>
-  <div class="container">
+<body style="height: 100vh">
+  <div class="container h-100 d-flex align-items-center justify-content-center">
     <?= $this->include('partials/navbar.php'); ?>
 
-    <?= $this->renderSection("content") ?>
+    <div class="w-100">
+      <?= $this->renderSection("content") ?>
+    </div>
   </div>
 
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -10,11 +10,15 @@
   }
 </style>
 
-<div id="carouselExampleDark" class="carousel carousel-dark slide p-5 text-center bg-light mx-auto d-block" style="border: 1px solid black; margin-top: 120px;">
+<div id="organization-carousel-mobile" class="d-block d-md-none" style="background-color: red;">
+  <h1>mobile</h1>
+</div>
+
+<div id="organization-carousel" class="carousel carousel-dark slide p-5 text-center bg-light mx-auto d-none d-md-block" style="border: 1px solid black;">
   <div class="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleDark" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    <button type="button" data-bs-target="#organization-carousel" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#organization-carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#organization-carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
   </div>
   <div class="carousel-inner mx-auto d-block">
     <h1>Explore Organizations</h1>
@@ -85,17 +89,17 @@
       </div>
     </div>
   </div>
-  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="prev">
+  <button class="carousel-control-prev" type="button" data-bs-target="#organization-carousel" data-bs-slide="prev">
     <span class="carousel-control-prev-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Previous</span>
   </button>
-  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleDark" data-bs-slide="next">
+  <button class="carousel-control-next" type="button" data-bs-target="#organization-carousel" data-bs-slide="next">
     <span class="carousel-control-next-icon" aria-hidden="true"></span>
     <span class="visually-hidden">Next</span>
   </button>
 </div>
-<div class="container text-center p-5" style="margin-top: 50px;">
+<div class="container text-center p-5" ">
 
-  <button class="btn btn-primary mx-auto d-block" type="button" style="width:30%;">Barter</button>
+  <button class=" btn btn-primary mx-auto d-block" type="button" style="width:30%;">Barter</button>
 </div>
 <?= $this->endSection() ?>
