00b2e0a85f44567443b49f24e09233d65a6641fa
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-06-15 01:39:17 +0800

    feat: improved layout for home page

--- app/Views/pages/home.php
+++ app/Views/pages/home.php
@@ -10,26 +10,23 @@
   }
 </style>
 
-<div class="row">
+<div class="row w-100 gy-5">
   <div class="col-12 text-center shadow-lg p-5">
-    <h1>Explore Organizations</h1>
+    <h1>Affiliate Organizations</h1>
     <div class="d-flex flex-row flex-nowrap overflow-auto">
       <?php foreach ($organizations as $organization) : ?>
-        <div class="card m-2" style="min-height: 200px; min-width: 250px; margin-right: 5px;">
+        <div class="card m-2 pt-3" style="min-height: 200px; min-width: 250px; margin-right: 5px;">
           <img src="<?= json_decode($organization->logo)->url ?>" class="img-thumbnail mx-auto d-block rounded-circle" alt="..." style="width:175px; height: 175px;">
           <div class="card-body">
             <h5 class="card-title"><?= esc($organization->name) ?></h5>
             <p><?= esc($organization->full_name) ?></p>
-            <a href="#" class="btn btn-primary">View Products</a>
           </div>
         </div>
       <?php endforeach; ?>
     </div>
   </div>
-</div>
-<div class="row mt-5">
   <div class="col-12 text-center">
-    <a href="<?= url_to("TestViewsController::viewBarter") ?>" class=" btn btn-primary mt-5 w-25 h-50" type="button">Barter</a>
+    <a href="<?= url_to("TestViewsController::viewBarter") ?>" class=" btn btn-primary " type="button" style="width: 250px; height: 50px;">Barter</a>
   </div>
 </div>
 
