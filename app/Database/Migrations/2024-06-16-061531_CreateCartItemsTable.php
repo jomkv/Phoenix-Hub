34d6355248919e14b1d491dfe7dc6724404ced7b
Jom Karlo Verzosa <jomkarlov@gmail.com>
2024-06-16 14:23:46 +0800

    feat: cart items migration, model and entity

---
+++ app/Database/Migrations/2024-06-16-061531_CreateCartItemsTable.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Database\Migrations;
+
+use CodeIgniter\Database\Migration;
+
+class CreateCartItemsTable extends Migration
+{
+    public function up()
+    {
+        $this->forge->addField([
+            'cart_item_id'  => [
+                'type'                  => 'INT',
+                'unsigned'              => true,
+                'auto_increment'        => true,
+                'null'                  => false,
+            ],
+            'student_id'       => [
+                'type'                  => 'INT',
+                'unsigned'              => true,
+                'null'                  => false,
+            ],
+            'product_id'     => [
+                'type'                  => 'INT',
+                'unsigned'              => true,
+                'null'                  => false,
+            ],
+            'quantity'       => [
+                'type'                  => 'INT',
+                'null'                  => false,
+            ],
+            'created_at'    => [
+                'type'                  => 'DATETIME',
+                'null'                  => true
+            ],
+            'updated_at'    => [
+                'type'                  => 'DATETIME',
+                'null'                  => true
+            ],
+            'deleted_at'    => [
+                'type'                  => 'DATETIME',
+                'null'                  => true
+            ],
+        ]);
+
+        $this->forge->addKey('cart_item_id', TRUE);
+        $this->forge->addForeignKey('student_id', 'users', 'id', 'CASCADE', 'CASCADE');
+        $this->forge->addForeignKey('product_id', 'products', 'product_id', 'CASCADE', 'CASCADE');
+        $this->forge->createTable('cart_items');
+    }
+
+    public function down()
+    {
+        $this->forge->dropTable('cart_items');
+    }
+}
